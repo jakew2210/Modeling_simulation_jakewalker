9c0b718b1b65fb25303a6a3a640579377373b53f
jakew2210 <jakewalker044@gmail.com>
2026-02-26 17:46:50 -0500

    Add Patient class with arrival time attribute

---
+++ src/Patient.java
@@ -0,0 +1,9 @@
+public class Patient {
+
+    public double arrivalTime;
+    public double serviceStartTime;
+
+    public Patient(double arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+}
