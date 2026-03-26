3656057a3e8468eaf5e81b8d3db18d2d6fc3aa4b
jakew2210 <jakewalker044@gmail.com>
2026-03-26 16:27:27 -0400

    Add queue statistics variables to Statistics class

--- src/Statistics.java
+++ src/Statistics.java
@@ -3,4 +3,7 @@ public class Statistics {
     public double totalWaitTime = 0;
     public int patientsServed = 0;
     public double totalBusyTime = 0;
+    public int maxQueueLength = 0;
+    public double queueArea = 0;
+    public double lastQueueUpdateTime = 0;
 }
